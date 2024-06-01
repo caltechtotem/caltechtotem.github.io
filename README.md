80d3cf1c9d4255a8d48b39bba54d215a4d44abe4
Totem <>
2024-06-01 17:46:20 +0300

    update

---
+++ README.md
@@ -0,0 +1,4 @@
+This is the content of Caltech's Totem website.
+
+This work is licensed under a [Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License](http://creativecommons.org/licenses/by-nc-nd/4.0/).
+
---
+++ gallery/js/main.js
@@ -0,0 +1,22 @@
+/*
+ * Author: Logan Apple
+ * Date: May 12th, 2021
+ */
+
+(function() {
+    /**
+     * Toggles the side menu view with the table of contents.
+     */   
+    /* function toggleView() {
+        document.getElementById("side-container").classList.toggle("hidden");
+    }
+
+    document.getElementById("menu-btn").addEventListener("click", toggleView);*/
+
+    easyScrollDots({
+        'fixedNav': false,
+        'fixedNavId': '',
+        'fixedNavUpward': false,
+        'offset': 0
+      });
+})();
